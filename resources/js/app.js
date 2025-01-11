a2bc668a17b9aef79374f88bcc96f33866513730
Carlos GonÃnonçalves <carlos.goncalves1193@gmail.com>
2025-01-10 21:46:50 -0300

    Add jquery and jquery-mask-plugin in the project

--- resources/js/app.js
+++ resources/js/app.js
@@ -2,6 +2,12 @@ import './bootstrap';
 
 import Alpine from 'alpinejs';
 
+import $ from 'jquery'
+
+import 'jquery-mask-plugin'
+
+window.$ = window.Jquery = $
+
 window.Alpine = Alpine;
 
 Alpine.start();
